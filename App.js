03913d7f0bb9f0045cad9d16d6ff604527a358ed
Georgy Mishin <>
2020-10-09 17:51:10 +0300

    Make simple drag-and-drop

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Animated, PanResponder } from "react-native";
 
-export default function App() {
+const App = () => {
+  const translationValue = React.useMemo(
+    () => new Animated.ValueXY({ x: 0, y: 0 }),
+    []
+  );
+
+  const opacityValue = React.useMemo(() => new Animated.Value(1), []);
+
+  const panResponder = React.useMemo(() => {
+    let offsetX = 0;
+    let offsetY = 0;
+    return PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        Animated.timing(opacityValue, {
+          toValue: 0.5,
+          useNativeDriver: true,
+          duration: 200,
+        }).start();
+      },
+      onPanResponderRelease: () => {
+        Animated.timing(opacityValue, {
+          toValue: 1,
+          useNativeDriver: true,
+          duration: 200,
+        }).start();
+      },
+      onPanResponderMove: (_, { dx, dy }) => {
+        translationValue.x.setValue(dx + offsetX);
+        translationValue.y.setValue(dy + offsetY);
+      },
+
+      onPanResponderEnd: (_, { dx, dy }) => {
+        translationValue.x.setValue(dx + offsetX);
+        translationValue.y.setValue(dy + offsetY);
+
+        offsetX += dx;
+        offsetY += dy;
+      },
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.canvas}>
+        <Animated.View
+          style={[
+            styles.cube,
+            { opacity: opacityValue },
+            {
+              transform: translationValue.getTranslateTransform(),
+            },
+          ]}
+          {...panResponder.panHandlers}
+        />
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  canvas: {
+    flex: 1,
+  },
+  cube: {
+    width: 150,
+    height: 150,
+    backgroundColor: "red",
+    borderRadius: 10,
   },
 });
